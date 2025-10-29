878c953cd584de6d26fb80d4f5639c64fcfd7267
KinjiKawaguchi <kawakin0310@icloud.com>
2025-10-29 19:36:52 +0900

    feat: IdentityLinkリポジトリを追加し、リポジトリのインポートを整理

--- src/lib.rs
+++ src/lib.rs
@@ -124,8 +124,11 @@ pub mod prelude {
             router::NotificationRouter,
             senders::{MockSender, SlackSender},
         },
-        repositories::resource_usage::{
-            google_calendar::GoogleCalendarUsageRepository, mock::MockUsageRepository,
+        repositories::{
+            identity_link::JsonFileIdentityLinkRepository,
+            resource_usage::{
+                google_calendar::GoogleCalendarUsageRepository, mock::MockUsageRepository,
+            },
         },
     };
 }
@@ -142,7 +145,10 @@ pub use infrastructure::{
         router::NotificationRouter,
         senders::{MockSender, SlackSender},
     },
-    repositories::resource_usage::{
-        google_calendar::GoogleCalendarUsageRepository, mock::MockUsageRepository,
+    repositories::{
+        identity_link::JsonFileIdentityLinkRepository,
+        resource_usage::{
+            google_calendar::GoogleCalendarUsageRepository, mock::MockUsageRepository,
+        },
     },
 };
